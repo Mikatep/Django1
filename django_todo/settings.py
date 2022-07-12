18ed9da9e912b5e106a5f39312572f9c3ef18a7b
Mikatep <miriamcordobacapote@gmail.com>
2022-07-12 10:50:33 +0000

    admin access sorted

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-mikatep-django1-iwgxaon7rnt.ws-eu53.gitpod.io']
